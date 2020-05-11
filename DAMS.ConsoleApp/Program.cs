8b758d7666e7d1f8437fdadd9a23d16d78e7c06d
OBorovenskyi <aleks.borovensky@gmail.com>
2020-05-11 23:12:32 +0300

    [O_Init] Creates Console App proj.

---
+++ DAMS.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Terminal.Gui;
+
+namespace DAMS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitApp();
+            Application.Run();
+        }
+
+        static void InitApp()
+        {
+            Application.Init();
+            var top = Application.Top;
+
+            var window = new Window("DAMS Console")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            top.Add(window);
+        }
+    }
+}
