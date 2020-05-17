cca9c8a3dbb50001b7a5976d87a40dd767743d67
andry5bb <andry5bb@gmail.com>
2020-05-17 14:37:16 +0300

    [A_sched] Creates class Scheduler.

---
+++ src/DAMS.Core/EventReminder/Scheduler/Scheduler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DAMS.EventReminder.Scheduler
+{
+    public class Scheduler : IScheduler
+    {
+        public void PrepareNotificationBucket(IEnumerable<IEvent> events)
+        {           
+            var now = DateTime.Now;
+            List<IEvent> notificationBucket = new List<IEvent>();
+
+            foreach (var currentEvent in events)
+            {
+                TimeSpan left = currentEvent.NextNotificationDate - now;               
+                if (left.Days == 0 && left.Hours == 0 && left.Minutes < 5 && left.Minutes >= 0 && left.Seconds > 0)
+                {
+                    notificationBucket.Add(currentEvent);                    
+                }
+            }
+        }
+    }
+}
