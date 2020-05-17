daffdc15086df13c0991687139c11d22c4ca2e27
andry5bb <andry5bb@gmail.com>
2020-05-17 14:24:45 +0300

    [A_sched] Creates Skeleton.

--- src/DAMS.Core/EventReminder/IEvent.cs
+++ src/DAMS.Core/EventReminder/IEvent.cs
@@ -1,7 +1,9 @@
-﻿namespace DAMS.EventReminder
+﻿using System;
+
+namespace DAMS.EventReminder
 {
     public interface IEvent
     {
-        
+        public DateTime NextNotificationDate { get; set; }
     }
 }
