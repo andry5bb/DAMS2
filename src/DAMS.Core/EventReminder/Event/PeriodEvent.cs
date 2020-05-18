d11d59bced4dced1cf51de9f66f57e52330af7c2
CapitanSA <mikhail.shulga.95@gmail.com>
2020-05-18 18:59:38 +0300

    [M_event] Creates Method GetNextNotificationDate.

--- src/DAMS.Core/EventReminder/Event/PeriodEvent.cs
+++ src/DAMS.Core/EventReminder/Event/PeriodEvent.cs
@@ -9,7 +9,7 @@ namespace DAMS.EventReminder.Event
     {
         public DateTime Date { get; set; }
         public PeriodType PeriodType { get; set; }
-        public DateTime NextNotificationDate { get { return Date - NotifyBefore; } }
+        public DateTime NextNotificationDate { get { return GetNextNotificationDate(); } }
         public string Name { get; set; }
         public TimeSpan NotifyBefore { get; set; }
         public INotifier Notifier { get; set; }
@@ -25,7 +25,75 @@ namespace DAMS.EventReminder.Event
             NotifyBefore = new TimeSpan(0, 5, 0);
             Status = EventStatus.Active;
         }
-
+        private DateTime GetNextNotificationDate()
+        {
+            DateTime NextDate = Date;
+            if (PeriodType == PeriodType.None)
+            {
+                NextDate = Date - NotifyBefore; ;
+            }
+            if (PeriodType == PeriodType.Daily)
+            {
+                if (NextDate - NotifyBefore > DateTime.Now)
+                {
+                    NextDate = NextDate - NotifyBefore;
+                }
+                else
+                {
+                    while (NextDate - NotifyBefore < DateTime.Now)
+                    {
+                        NextDate = NextDate.AddDays(1);
+                    }
+                    NextDate = NextDate - NotifyBefore;
+                }
+            }
+            if (PeriodType == PeriodType.Weekly)
+            {
+                if (NextDate - NotifyBefore > DateTime.Now)
+                {
+                    NextDate = NextDate - NotifyBefore;
+                }
+                else
+                {
+                    while (NextDate - NotifyBefore < DateTime.Now)
+                    {
+                        NextDate = NextDate.AddDays(7);
+                    }
+                    NextDate = NextDate - NotifyBefore;
+                }
+            }
+            if (PeriodType == PeriodType.Monthly)
+            {
+                if (NextDate - NotifyBefore > DateTime.Now)
+                {
+                    NextDate = NextDate - NotifyBefore;
+                }
+                else
+                {
+                    while (NextDate - NotifyBefore < DateTime.Now)
+                    {
+                        NextDate = NextDate.AddMonths(1);
+                    }
+                    NextDate = NextDate - NotifyBefore;
+                }
+            }
+            if (PeriodType == PeriodType.Yearly)
+            {
+                if (NextDate - NotifyBefore > DateTime.Now)
+                {
+                    NextDate = NextDate - NotifyBefore;
+                }
+                else
+                {
+                    while (NextDate - NotifyBefore < DateTime.Now)
+                    {
+                        NextDate = NextDate.AddYears(1);
+                    }
+                    NextDate = NextDate - NotifyBefore;
+                }
+            }
+            return NextDate;
+        }
         public PeriodEvent(INotifier notifier, DateTime date, string name, TimeSpan time, EventStatus status)
         {
             Notifier = notifier;
