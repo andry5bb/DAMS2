8f0d5b03e9db680dd6647fdeb5da741472201cce
andry5bb <andry5bb@gmail.com>
2020-05-19 21:25:41 +0300

    [A_bucket] fix bug.

---
+++ src/DAMS.Core/EventReminder/Scheduler/NotificationBucket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAMS.EventReminder.Scheduler
+{
+    public class NotificationBucket
+    {
+        public IEnumerable<IEvent> NextEvents { get; set; }
+
+        internal void Add(IEvent currentEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public NotificationBucket (IEnumerable<IEvent> nextEvent)
+        //{
+        //    NextEvents = nextEvent;
+        //}
+    }
+}
