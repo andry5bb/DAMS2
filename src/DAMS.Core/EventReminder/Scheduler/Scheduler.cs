4b6044999dec6e10cdcbacd082f9811a1ca07bcb
andry5bb <andry5bb@gmail.com>
2020-06-01 15:17:09 +0300

    [A_sched] Removed whitespace.

--- src/DAMS.Core/EventReminder/Scheduler/Scheduler.cs
+++ src/DAMS.Core/EventReminder/Scheduler/Scheduler.cs
@@ -12,8 +12,8 @@ namespace DAMS.EventReminder.Scheduler
 
             foreach (var currentEvent in events)
             {
-                TimeSpan left = currentEvent.NextNotificationDate - now ;
-                if (left.Minutes <  5 && left.Minutes >= 0)
+                TimeSpan left = currentEvent.NextNotificationDate - now;
+                if (left.Minutes < 5 && left.Minutes >= 0)
                 {
                     bucket.Add(currentEvent);
                 }
