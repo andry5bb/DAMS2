7adb4501a9beb55e8d42b2a1fd80f209bc3a6bac
CapitanSA <mikhail.shulga.95@gmail.com>
2020-06-04 22:41:59 +0300

    [M_Telegram] Clean up.

--- DAMS.Telegram/Program.cs
+++ DAMS.Telegram/Program.cs
@@ -13,21 +13,16 @@ namespace DAMS.Telegram
 {
     class Program
     {
-        static ITelegramBotClient botClient;//Оголошуємо телеграм клієнта
-        //
-
+        static ITelegramBotClient botClient;//Оголошуємо телеграм 
         static void Main(string[] args)
         {//ініціалізуємо нашого бота, та в якості параметра передаємо конструктору токен бота DAMSbot
             botClient = new TelegramBotClient("1203508145:AAFYSqqz66zVYJ9xP5MU_kr0JwZDprIhiQE");
-
             MyMessage myMessage = new MyMessage();
             myMessage.BotFirstWriteMessage("BotFirstWrite");
             myMessage.SendMyMessage(botClient, "ghhghgh");
             Console.ReadKey();
-            
-        }
 
-       
+        }
     }
        
     }
