bc601affb3fe6daac9f11b30d4a5aef08d109a78
CapitanSA <mikhail.shulga.95@gmail.com>
2020-06-04 22:33:07 +0300

    [M_Telegram] Creates Telegram sceleton.

---
+++ DAMS.Telegram/MyMessage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using System.Threading;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using System.Net;
+
+namespace DAMS.Telegram
+{
+    public class MyMessage
+    {
+
+        public void SendMyMessage(ITelegramBotClient client, string mytext)
+        {
+            client.OnMessage += BotClient_OnMessage;
+            client.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+            client.OnMessage += BotClient_OnMessage;
+
+            async void BotClient_OnMessage(object sender, MessageEventArgs e)
+            {
+                var keyboard = new ReplyKeyboardMarkup();
+                var buttons = new List<KeyboardButton[]>();
+                var button = new List<KeyboardButton>();
+                button.Add(new KeyboardButton("My one time event"));
+                button.Add(new KeyboardButton("My custon event"));
+                button.Add(new KeyboardButton("My period event"));
+                buttons.Add(button.ToArray());
+                keyboard.Keyboard = buttons.ToArray();
+                if (e.Message.Text != null)
+                {
+                    if (e.Message.Text == "My one time event")
+                    {
+                        await client.SendTextMessageAsync
+                        (
+                        chatId: e.Message.Chat,
+                        text: "name"+ DateTime.Now,
+                        replyMarkup: keyboard
+                        );
+                    }
+                    if (e.Message.Text == "My custon event")
+                    {
+                        await client.SendTextMessageAsync
+                        (
+                        chatId: e.Message.Chat,
+                        text: "There should be information about CustomEvent type",
+                        replyMarkup: keyboard
+                        );
+                    }
+                    if (e.Message.Text == "My period event")
+                    {
+                        await client.SendTextMessageAsync
+                            (chatId: e.Message.Chat,
+                            text: "There should be information about PeriodEvent type ",
+                            replyMarkup: keyboard
+                            );
+
+                    }
+
+                    }
+                    await client.SendTextMessageAsync
+                            (chatId: e.Message.Chat,
+                            text:mytext,
+                            replyMarkup: keyboard
+                            );
+
+                }
+            }
+
+        
+        public void BotFirstWriteMessage(string text)
+        { 
+            WebRequest request = WebRequest.Create("https://api.telegram.org/bot1203508145:AAFYSqqz66zVYJ9xP5MU_kr0JwZDprIhiQE/sendMessage?chat_id=457253196&text=" + text);
+            request.GetResponse();
+        }
+      
+
+       
+
+    }
+}
--- DAMS.Telegram/Program.cs
+++ DAMS.Telegram/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using RestSharp;
+
 
 namespace DAMS.Telegram
 {
     class Program
     {
+        static ITelegramBotClient botClient;//Оголошуємо телеграм клієнта
+        //
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {//ініціалізуємо нашого бота, та в якості параметра передаємо конструктору токен бота DAMSbot
+            botClient = new TelegramBotClient("1203508145:AAFYSqqz66zVYJ9xP5MU_kr0JwZDprIhiQE");
+
+            MyMessage myMessage = new MyMessage();
+            myMessage.BotFirstWriteMessage("BotFirstWrite");
+            myMessage.SendMyMessage(botClient, "ghhghgh");
+            Console.ReadKey();
+            
         }
+
+       
     }
-}
+       
+    }
+
