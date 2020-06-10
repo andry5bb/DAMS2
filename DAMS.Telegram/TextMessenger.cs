611da4c6c1c43c2b0e78de32237f430c4fbb4114
CapitanSA <mikhail.shulga.95@gmail.com>
2020-06-10 17:55:25 +0300

    [M_Telegram] Removed lines and replaced string literals with DefaultNames class constants. Renamed method "BotFirstWrite Message" to "PushMessage" and added documentation about this method.

--- DAMS.Telegram/TextMessenger.cs
+++ DAMS.Telegram/TextMessenger.cs
@@ -9,33 +9,31 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
 using System.Net;
 using RestSharp;
+using DAMS.NotificationSystems.All;
 
 namespace DAMS.Telegram
 {
     public class TextMessenger
     {
         static ITelegramBotClient client = new TelegramBotClient("1203508145:AAFYSqqz66zVYJ9xP5MU_kr0JwZDprIhiQE");
-
         public void SendMyMessage(string mytext)
         {
             client.OnMessage += BotClient_OnMessage;
             client.StartReceiving();
-            Thread.Sleep(int.MaxValue);
-            client.OnMessage += BotClient_OnMessage;
 
             async void BotClient_OnMessage(object sender, MessageEventArgs e)
             {
                 var keyboard = new ReplyKeyboardMarkup();
                 var buttons = new List<KeyboardButton[]>();
                 var button = new List<KeyboardButton>();
-                button.Add(new KeyboardButton("My one time event"));
-                button.Add(new KeyboardButton("My custon event"));
-                button.Add(new KeyboardButton("My period event"));
+                button.Add(new KeyboardButton(DefaultNames.one_time_event));
+                button.Add(new KeyboardButton(DefaultNames.custom_event));
+                button.Add(new KeyboardButton(DefaultNames.period_event));
                 buttons.Add(button.ToArray());
                 keyboard.Keyboard = buttons.ToArray();
                 if (e.Message.Text != null)
                 {
-                    if (e.Message.Text == "My one time event")
+                    if (e.Message.Text == DefaultNames.one_time_event)
                     {
                         await client.SendTextMessageAsync
                         (
@@ -44,7 +42,7 @@ namespace DAMS.Telegram
                         replyMarkup: keyboard
                         );
                     }
-                    if (e.Message.Text == "My custon event")
+                    if (e.Message.Text == DefaultNames.custom_event)
                     {
                         await client.SendTextMessageAsync
                         (
@@ -53,7 +51,7 @@ namespace DAMS.Telegram
                         replyMarkup: keyboard
                         );
                     }
-                    if (e.Message.Text == "My period event")
+                    if (e.Message.Text == DefaultNames.period_event)
                     {
                         await client.SendTextMessageAsync
                             (chatId: e.Message.Chat,
@@ -70,13 +68,19 @@ namespace DAMS.Telegram
                 }
             }
         }
-        public void BotFirstWriteMessage(string chat_id, string text_message)
+
+        /// <summary>
+        ///  Method that performs notifications via Telegram.
+        /// </summary>
+        /// <param name="chat_id">The ID of the chat to which the bot will send notifications. </param>
+        /// <param name="text_message">Notification text.</param>
+        public void PushMessage(string chat_id, string text_message)
         {
             var client = new RestClient("https://api.telegram.org");
             var request = new RestRequest("https://api.telegram.org/bot1203508145:AAFYSqqz66zVYJ9xP5MU_kr0JwZDprIhiQE/sendMessage?chat_id=" + chat_id + "&text=" + text_message);
             client.Execute(request);
 
         }
-
+   
     }
 }
