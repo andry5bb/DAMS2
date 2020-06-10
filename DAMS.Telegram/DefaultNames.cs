b21e9d95afdf71165e208f4ae1cd391bb2f06ee7
CapitanSA <mikhail.shulga.95@gmail.com>
2020-06-10 17:46:04 +0300

    [M_Telegram] Creates public class DefaultNames which contains default names for our events.

---
+++ DAMS.Telegram/DefaultNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAMS.NotificationSystems.All
+{
+    public class DefaultNames
+    {
+        public const string one_time_event = "One Time Event";
+        public const string custom_event = "Custom Event";
+        public const string period_event = "Period Event";
+    }
+}
