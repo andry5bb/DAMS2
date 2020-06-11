787895842ba78c5830a278e137acd934ee2f399f
CapitanSA <mikhail.shulga.95@gmail.com>
2020-06-11 21:36:52 +0300

    [M_Telegram] Rename private method "MessageContent" to "GetMessageContent" and method "SendMyMessage" to "SendMessage".

--- DAMS.Email/Telegram/TelegramNotifier.cs
+++ DAMS.Email/Telegram/TelegramNotifier.cs
@@ -3,21 +3,23 @@ using DAMS.EventReminder.Notifier;
 using DAMS.Telegram;
 
 namespace DAMS.NotificationSystems.All.Telegram
-{ 
+{
     public class TelegramNotifier : INotifier
-    { private string MessageContent(EventInfo eventInfo)
+    {
+        private string GetMessageContent(EventInfo eventInfo)
         {
             string message = eventInfo.EventName + "  " + eventInfo.EventDate;
             return message;
         }
+
         public NotificationResult Notify(EventInfo eventInfo)
         {
             TextMessenger messenger = new TextMessenger();
             NotificationResult result = new NotificationResult();
-            
+
             try
             {
-                messenger.SendMyMessage(MessageContent(eventInfo)); 
+                messenger.SendMessage(GetMessageContent(eventInfo));
                 result.IsSuccess = true;
             }
             catch
@@ -26,13 +28,14 @@ namespace DAMS.NotificationSystems.All.Telegram
             }
             return result;
         }
+
         public NotificationResult Notify(EventInfo eventInfo, string chat_id)
         {
             TextMessenger textMessange = new TextMessenger();
             NotificationResult result = new NotificationResult();
             try
             {
-                textMessange.PushMessage(chat_id, MessageContent(eventInfo));
+                textMessange.PushMessage(chat_id, GetMessageContent(eventInfo));
 
                 result.IsSuccess = true;
             }
