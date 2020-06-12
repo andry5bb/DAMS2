0137839b1ee9852887b9158042e20dc2e6629c75
andry5bb <andry5bb@gmail.com>
2020-06-12 19:32:07 +0300

    [A_email] added 2 private class for smtp and mailMessage.

--- DAMS.Email/Email/EmailNotifier.cs
+++ DAMS.Email/Email/EmailNotifier.cs
@@ -5,10 +5,11 @@ using DAMS.EventReminder.Notifier;
 using System.IO;
 using System.Threading.Tasks;
 using System.Net.Mail;
-using EO.Internal;
 using DAMS.EventReminder.Event;
 using System.Configuration;
 using System.Collections.Specialized;
+using DAMS.NotificationSystems.All.Email;
+using Newtonsoft.Json;
 
 // 3 окремий приватний метод припеа емейл баді(контент) в 
 //якому буде міститися логіка як робиться мейл контент (знайти шаблом email event notification template)
@@ -19,45 +20,12 @@ namespace DAMS.NotificationSystems.All.Email
     public class EmailNotifier : INotifier
     {
         public NotificationResult Notify(NotificationInfo eventInfo)
-        {
-            string mailFrom = ConfigurationManager.AppSettings.Get("fromMailAddress");
-            // відправник - встановлюємо адресу і відображається в листі ім'я
-            MailAddress fromMailAddress = new MailAddress(mailFrom);
-            // кому відправляємо
-            MailAddress toAddress = new MailAddress(eventInfo.EmailRecipient);
-            // створюємо об'єкт повідомлення
-            MailMessage mailMessage = new MailMessage(fromMailAddress, toAddress);
-            // Тема листа
-            mailMessage.Subject = "Нагадування про подію!";
-            // текст листа
-            mailMessage.Body = eventInfo.EventName + eventInfo.EventDate; // окремий приватний метод припеа емейл баді(контент) в 
-            //якому буде міститися логіка як робиться мейл контент (знайти шаблом email event notification template)
-            //знайти html шаблон і змінити в ньому 2-3 місця (ім'я, дату і т.д.)
-            // лист представляє код html / прапор використання html
-            mailMessage.IsBodyHtml = true;
-            // вкладений файл
-            //mailMessage.Attachments.Add(new Attachment("C:/Users/Андрей/Desktop"));
-            string smtpHost = ConfigurationManager.AppSettings.Get("Host_smtp");
-            string login = ConfigurationManager.AppSettings.Get("login");
-            string password = ConfigurationManager.AppSettings.Get("password");
-            // адрес smtp-сервера і порт, з якого будемо відправляти повідомлення
-            var smtp = new SmtpClient
-            {
-                Host = smtpHost,
-                Port = 25,
-                //встановлюємо при необхідності SSL
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                // логін та пароль
-                Credentials = new NetworkCredential(login, password),
-            };
+        {            
+            var a = PrepareSmtpClient();
             NotificationResult result = new NotificationResult();
-            //спроба відправки повідомлення
             try
             {
-                smtp.Send(mailMessage);
-                // якщо вдало, то встановлюємо статус
+                a.Send(FormEmail(eventInfo));
                 result.IsSuccess = true;
             }
             catch 
@@ -65,6 +33,34 @@ namespace DAMS.NotificationSystems.All.Email
                 result.Details = "Error";
             }
             return result; 
-        } 
+        }
+        private MailMessage FormEmail(NotificationInfo eventInfo)
+        {
+            string mailFrom = ConfigurationManager.AppSettings.Get("fromMailAddress");
+            string NameNotifier = ConfigurationManager.AppSettings.Get("NameAdmin");
+            MailAddress fromMailAddress = new MailAddress(mailFrom, NameNotifier);
+            MailAddress toAddress = new MailAddress(eventInfo.EmailRecipient);
+            MailMessage mailMessage = new MailMessage(fromMailAddress, toAddress);
+            mailMessage.Subject = "Нагадування про подію!";
+            mailMessage.Body = eventInfo.EventName + eventInfo.EventDate; 
+            mailMessage.IsBodyHtml = true;
+            return mailMessage;
+        }
+        private SmtpClient PrepareSmtpClient()
+        {
+            string smtpHost = ConfigurationManager.AppSettings.Get("Host_smtp");
+            string login = ConfigurationManager.AppSettings.Get("login");
+            string password = ConfigurationManager.AppSettings.Get("password");
+            var smtp = new SmtpClient
+            {
+                Host = smtpHost,
+                Port = 25,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(login, password),
+            };
+            return smtp;
+        }
     }
 }
