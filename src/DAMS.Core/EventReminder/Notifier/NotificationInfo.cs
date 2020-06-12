5f6acbe3e68e65684f5568541b73743d19c33911
andry5bb <andry5bb@gmail.com>
2020-06-12 19:35:04 +0300

    [A_email] changed struct -> class. Added field emailTest and added in field EmailRecipient {set and get } property.

--- src/DAMS.Core/EventReminder/Notifier/NotificationInfo.cs
+++ src/DAMS.Core/EventReminder/Notifier/NotificationInfo.cs
@@ -1,51 +1,44 @@
-﻿using System;
-using System.Net.Mail;
+﻿using DAMS.Helpers;
+using System;
 using System.Text.RegularExpressions;
 
 namespace DAMS.EventReminder.Notifier
 {
-    public struct NotificationInfo
+    public class NotificationInfo
     {
         public string EventName { get; }
         public DateTime EventDate { get; }
-        public string EmailRecipient { get; set; }
-
+        private string emailTest;
+        public RegexValidator regex = new RegexValidator();
+        public string EmailRecipient
+        {
+            get
+            {
+                return emailTest;
+            }
+            set
+            {
+                if (regex.RegEx(value) == true)
+                {
+                    emailTest = value;
+                }
+                if (regex.RegEx(value) == false)
+                {
+                    //throw new ArgumentException("email");
+                    //var user = GetUse(value);
+                    throw new ArgumentException(String.Format("{0} Invalid email input format ", value), "email");
+                    //System.ArgumentException argEx = new System.ArgumentException("Index is out of range", "index", e);
+                    //throw argEx;
+                }
+            }
+        }
 
 
         public NotificationInfo(string eventName, DateTime eventDate, string emailRecipient)
         {
             EventName = eventName;
             EventDate = eventDate;
-            // TODO : Add validation with regex
             EmailRecipient = emailRecipient;
-        }
-        public void RegEX (string emailRecipient)
-        {
-        //test:
-            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-          @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
-            // статичний метод IsMatch, який дозволяє перевірити вхідні рядок з шаблоном на відповідність:
-            // Змінна pattern задає регулярний вираз для перевірки адреси електронної пошти.
-            // Цей вираз пропонує нам Microsoft на сторінках msdn.
-            //Console.ForegroundColor = ConsoleColor.Blue;
-            //Console.WriteLine("МЕТОД 2 Введiть адресу електронної пошти: ");
-            //Console.ResetColor();
-            emailRecipient = Console.ReadLine();
-           
-                if (Regex.IsMatch(emailRecipient, pattern, RegexOptions.IgnoreCase))
-                {
-                //Console.ForegroundColor = ConsoleColor.Green;
-                //Console.WriteLine("Email подтвержден");
-                //Console.ResetColor();
-                EmailRecipient = emailRecipient;
-                }
-                else
-                {
-                    //Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Некорректный email");
-                    //Console.ResetColor();
-                }
-            //goto test;
         }
     }
 }
