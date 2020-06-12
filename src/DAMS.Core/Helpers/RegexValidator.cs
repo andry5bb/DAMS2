0ab6d5527dfcf1dea55f1585730f19d6109af623
andry5bb <andry5bb@gmail.com>
2020-06-12 19:35:54 +0300

    [A_email] added method RegEx for emailValidation.

--- src/DAMS.Core/Helpers/RegexValidator.cs
+++ src/DAMS.Core/Helpers/RegexValidator.cs
@@ -1,7 +1,24 @@
-﻿namespace DAMS.Helpers
+﻿using System;
+using System.Text.RegularExpressions;
+using DAMS.EventReminder.Notifier;
+
+namespace DAMS.Helpers
 {
     public class RegexValidator
     {
-        
+        public bool RegEx(string emailRecipient)
+        {
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+            @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            if (Regex.IsMatch(emailRecipient, pattern, RegexOptions.IgnoreCase))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
