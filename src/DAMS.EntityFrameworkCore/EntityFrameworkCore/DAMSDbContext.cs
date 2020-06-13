29e88169f0b2d828a73e835725d8b1f10d4e8342
OBorovenskyi <aleks.borovensky@gmail.com>
2020-06-13 22:43:54 +0300

    [O_db]

--- src/DAMS.EntityFrameworkCore/EntityFrameworkCore/DAMSDbContext.cs
+++ src/DAMS.EntityFrameworkCore/EntityFrameworkCore/DAMSDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.EntityFrameworkCore;
+using DAMS.EventReminder.Event;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAMS.EntityFrameworkCore
@@ -6,11 +7,13 @@ namespace DAMS.EntityFrameworkCore
     public class DAMSDbContext : AbpDbContext
     {
         //Add DbSet properties for your entities...
+        public virtual DbSet<OneTimeEvent> OneTimeEvents { get; set; } 
+
 
         public DAMSDbContext(DbContextOptions<DAMSDbContext> options) 
             : base(options)
         {
-
+            
         }
     }
 }
