7a4e88c677d1337868fc6c98f4d6ded3873aa693
andry5bb <andry5bb@gmail.com>
2020-06-13 17:17:01 +0300

    [A_email] Renamed class.

--- src/DAMS.Core/Helpers/RegexValidator.cs
+++ src/DAMS.Core/Helpers/RegexValidator.cs
@@ -4,7 +4,7 @@ namespace DAMS.Helpers
 {
     public class RegexValidator
     {
-        public bool RegExEmail(string emailRecipient)
+        public bool Email(string emailRecipient)
         {
             string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
             @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
